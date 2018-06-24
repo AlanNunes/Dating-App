ef0ef44a3c9415621e8eed3577621007526270c4
AlanNunes <alann.625@gmail.com>
2018-06-24 02:25:30 -0300

    Add GUI for Regitering Accounts

---
+++ RegisterAccount.php
@@ -0,0 +1,87 @@
+<html lang="pt">
+<head>
+	<!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+ 	<!-- <link rel="shortcut icon" type="image/x-icon" href="assets/imgs/logo/fav.ico"> -->
+	<title> Dating App - Register Account </title>
+
+	<!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body style="background: #fff9f7;">
+  <div class="panel">
+    <form>
+      <div class="form-row">
+        <div class="form-group col-md-6">
+          <label for="email">Email</label>
+          <input type="email" class="form-control" id="email" placeholder="Email">
+        </div>
+        <div class="form-group col-md-6">
+          <label for="password">Password</label>
+          <input type="password" class="form-control" id="password" placeholder="Password">
+        </div>
+      </div>
+      <div class="form-row">
+        <div class="form-group col-md-6">
+          <label for="firstName">First Name</label>
+          <input type="text" class="form-control" id="firstName" placeholder="First Name">
+        </div>
+        <div class="form-group col-md-6">
+          <label for="LastName">Last Name</label>
+          <input type="text" class="form-control" id="LastName" placeholder="Last Name">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <label for="age">Age</label>
+          <input type="number" class="form-control" id="age" placeholder="Age">
+        </div>
+        <div class="col">
+          <label for="gender">Gender</label>
+          <select id="gender" class="form-control">
+            <option value='m'>Man</option>
+            <option value='f'>Woman</option>
+          </select>
+        </div>
+      </div>
+      <br/>
+      <p>I'm interested in</p>
+      <div class="row">
+        <div class="col">
+          <div class="checkbox">
+            <label>
+              <input type="checkbox"> Men
+            </label>
+          </div>
+        </div>
+        <div class="col">
+          <div class="checkbox">
+            <label>
+              <input type="checkbox"> Women
+            </label>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <input type="phone" class="form-control" id="phone" placeholder="Phone">
+        </div>
+      </div>
+      <br/>
+      <p>
+        <button type="button" class="btn btn-primary btn-block" style="background: #ff5d4f;">Ready !</button>
+      </p>
+    </form>
+  </div>
+</body>
+<!-- Scripts -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script>
+
+    </script>
+</html>
